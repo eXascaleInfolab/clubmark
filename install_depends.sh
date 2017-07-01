3d3909aa06e611e3550ff581b2bf249ecb0a987a
luav <luardev@gmail.com>
2017-07-01 13:29:05 +0200

    Dependencies installation refined

--- install_depends.sh
+++ install_depends.sh
@@ -2,20 +2,25 @@
 # Install all external libraries
 # Note: this script is designed for Linux Ubuntu and might also work on Debian
 
-# Install  libstdc++.so.6
-#
-# libstdc++.so.6: version GLIBCXX_3.4.20  for hirecs and modularity evaluation is require in Ubuntu 14.04.
-# degault libstdc++6 is fine on Ubuntu 16.04+
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-
 sudo apt-get -y update
+
+# Get errorcode of the last operation and terminate if the update failed
+ERR=$?
+if [ $ERR -ne 0 ]
+then
+	echo "ERROR, the dependencies installation terminated, \"apt-get update\" failed with the code $ERR"
+	exit $ERR
+fi
+
+# Note: libstdc++6 version GLIBCXX_3.4.20+ is required only on the outdated Ubuntu (before 16.04)
 sudo apt-get -y install libstdc++6
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 
 sudo apt-get -y install pypy
 # TODO: install pip into pypy and python-igraph from the pypy_pip
 
 # python-igraph for Louvain execution with NMI-compatible results output
-sudo apt-get install libxml2-dev zlib1g-dev  # Required by python-igraph
+sudo apt-get -y install libxml2-dev zlib1g-dev  # Required by python-igraph
 sudo pip install python-igraph
 
 # Install Java for GaNXIS
